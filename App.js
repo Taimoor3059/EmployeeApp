de426f8176999313ccf45549830d7a73b75ab131
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-03-12 03:21:20 -0500

    actions

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { CreateStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import EmployeeApp from './src/EmployeeApp';
 
@@ -40,37 +39,38 @@ function reducer (state= initState, action) {
   let employeeObject;
 
   if (action.id) {
-    initialSalary = state[action.id].empSalary
+    initialSalary = state[action.id].empSalary;
+    increment = (initialSalary / 100) * 20;
   }
 
 
   switch (action.type) {
-    case value:
-      
-      break;
-  
-    default:
-      break;
+    case 'GOOD_PERFORMANCE':
+      newSalary = initialSalary + increment;
+      employeeObject = state[action.id];
+      employeeObject.empSalary = newSalary;
+      return Object.assign({}, state)
+    case 'BAD_PERFORMANCE':
+      newSalary = initialSalary - increment;
+      employeeObject = state[action.id];
+      employeeObject.empSalary = newSalary;
+      return Object.assign({}, state)
   }
+  return state;
 }
+
+const store = createStore(reducer);
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <EmployeeApp />
+      </Provider>
     );
   }
   
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
